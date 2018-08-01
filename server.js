74f1aed5192475deb44de43b8e37ff898ba51db0
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-08-01 22:35:57 +0000

    Initial express server with route files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+// DB config
+const db = require("./config/keys").mongoURL;
+// connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("<h1>Hello DevNet</h1>"));
+
+// Use routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+app.listen(port, () => console.log(`App started on port ${port}`));
