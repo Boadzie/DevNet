113024f37cc71771b184159b2d1ec225197c380d
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-08-12 15:29:05 +0000

    Added Client side of the app with all files

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Passport middleware
 app.use(passport.initialize());
