a0ebc4e72579b16efa815bc4513b2d704b9a3154
Boadzie Daniel <boadziedaniel43@gmail.com>
2018-08-23 20:56:36 +0000

    Prepared for production

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const app = express();
 
@@ -34,5 +35,14 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Serve Static assets when in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>{
+     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 app.listen(port, () => console.log(`App started on port ${port}`));
